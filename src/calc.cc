be01e7dcfc4d843542054b446a28ceec93855ced
ultratypebot <xxperipsis@gmail.com>
2018-04-09 19:47:15 -0500

    add test case as main

--- src/calc.cc
+++ src/calc.cc
@@ -1,4 +1,10 @@
 #include "Parser.h"
 #include "AST.h"
+#define EXPRESSION "1+9-3"
 
-int main(){}
+int main() {
+	ast::AST tree = ast::AST();
+	Parser p(EXPRESSION);
+	p.tree = &tree;
+	return 0;
+}
