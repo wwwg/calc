e91a1bb9e2446aece34a8f332203a45e5effb1ef
wwwg <xxperipsis@gmail.com>
2018-04-11 19:08:59 -0500

    add ast put functions

--- src/util/putast.cc
+++ src/util/putast.cc
@@ -1,5 +1,59 @@
 #include "putast.h"
 
+void ast::indent(int indents) {
+    for (int i = 0; i < indents; ++i) cout << "\t";
+}
+void ast::put(Constant* c, int ind) {
+    indent(ind);
+    cout << "Constant '" << c->value << "'" << endl; 
+}
+void ast::put(Operator* o, int ind) {
+    char op = ' ';
+    switch (o->operation) {
+        case Operation::Add:
+            op = '+';
+            break;
+        case Operation::Sub:
+            op = '-';
+            break;
+        case Operation::Mul:
+            op = '*';
+            break;
+        case Operation::Div:
+            op = '/';
+            break;
+        case Operation::Pow:
+            op = '^';
+            break;
+    }
+    indent(ind);
+    cout << "Operator '" << op << "' :" << endl;
+    indent(ind + 1);
+    cout << "Left:" << endl;
+    put(o->left, ind + 2);
+    cout << "Right:" << endl;
+    put(o->right, ind + 2);
+    cout << endl;
+}
+void ast::put(Block* b, int ind) {
+    indent(ind);
+    cout << "Block (";
+    for (int i = 0; i < b->list.size(); ++i) {
+        Expression* e = b->list.at(i);
+        put(e, ind + 1);
+    }
+    indent(ind);
+    cout << ")" << endl;
+}
+void ast::put(Expression* e, int ind) {
+    if (e->is<Constant>()) {
+        put(e->cast<Constant>(), ind);
+    } else if (e->is<Operator>()) {
+        put(e->cast<Operator>(), ind);
+    } else if (e->is<Block>()) {
+        put(e->cast<Block>(), ind);
+    }
+}
 void ast::put(Expression* e) {
-    //
+    put(e, 0);
 }
--- src/util/putast.h
+++ src/util/putast.h
@@ -3,9 +3,17 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <iostream>
 
 #include "../AST.h"
+using namespace std;
 
 namespace ast {
+    void indent(int);
+    void put(Constant*, int);
+    void put(Operator*, int);
+    void put(Block*, int);
+    
+    void put(Expression*, int);
     void put(Expression*);
 }
