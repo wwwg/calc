1ab4369d368a7964a94bff5d50d2bc6d811b563e
wwwg <xxperipsis@gmail.com>
2018-04-12 11:19:04 -0500

    initialize code holder

--- src/evaluators/jit.cc
+++ src/evaluators/jit.cc
@@ -5,6 +5,7 @@ eval::JitEvaluator::JitEvaluator(ast::AST* _tree) {
     runtime = new JitRuntime();
     code = new CodeHolder();
     assembler = new X86Assembler(code);
+    code->init(runtime->getCodeInfo());
 }
 void eval::JitEvaluator::generate() {
     runtime->add(&fn, code);
